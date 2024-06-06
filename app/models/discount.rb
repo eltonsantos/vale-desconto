bbb2d738b5e36aa9bae0261a4acc7bd5009ca28d
Elton Santos <eltonaxl@hotmail.com>
2024-06-06 02:44:26 -0300

    Finish upload image

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,7 @@
 class Discount < ApplicationRecord
   enum discount_type: { de_por: 0, leve_mais_pague_menos: 1, percentual: 2 }
 
+  has_one_attached :image
+
   validates :name, :description, :discount_type, :activation_date, :deactivation_date, presence: true
 end

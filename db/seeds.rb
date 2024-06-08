87c88384c55b67984b69e02766452f5585a9a476
Elton Santos <eltonaxl@hotmail.com>
2024-06-08 01:06:27 -0300

    Fix seed to storage images

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ users = User.create!([{email: "admin@admin.com", password: 123456}])
 
 puts "Criando produtos"
 products = Product.create!([
-  { title: "Produto 1", description: "Descrição do Produto 1", price: 10.0, image: "" }
+  { title: "Produto 1", description: "Descrição do Produto 1", price: 10.0 }
 ])
 
 puts "Criando descontos"
@@ -11,7 +11,6 @@ Discount.create([
   {
     name: "Biscoito integral cereale Cacau 150g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 0,
     activation_date: "2024-06-06 17:34:00",
     deactivation_date: "2024-06-11 17:34:00",
@@ -24,7 +23,6 @@ Discount.create([
   {
     name: "Biscoito integral cereale Cacau 200g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 0,
     activation_date: "2024-06-01 17:34:00",
     deactivation_date: "2024-06-15 17:34:00",
@@ -35,22 +33,8 @@ Discount.create([
     user: users[0]
   },
   {
-    name: "Biscoito integral cereale Cacau 200g",
-    description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
-    discount_type: 0,
-    activation_date: "2024-06-05 17:34:00",
-    deactivation_date: "2024-06-13 17:34:00",
-    status: 1,
-    price_from: 5.6,
-    price_to: 5.1,
-    product: products[0],
-    user: users[0]
-  },
-  {
     name: "Biscoito cereale Cacau 250g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 1,
     activation_date: "2024-06-02 17:34:00",
     deactivation_date: "2024-06-10 17:34:00",
@@ -64,7 +48,6 @@ Discount.create([
   {
     name: "Biscoito cereale Cacau 150g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 1,
     activation_date: "2024-06-07 17:34:00",
     deactivation_date: "2024-06-12 17:34:00",
@@ -76,23 +59,8 @@ Discount.create([
     user: users[0]
   },
   {
-    name: "Biscoito cereale Cacau 450g",
-    description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
-    discount_type: 1,
-    activation_date: "2024-06-06 17:34:00",
-    deactivation_date: "2024-06-18 17:34:00",
-    status: 1,
-    price: 17.99,
-    quantity_leve: 4,
-    quantity_pague: 3,
-    product: products[0],
-    user: users[0]
-  },
-  {
     name: "Biscoito cereale Cacau 90g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 2,
     activation_date: "2024-06-02 17:34:00",
     deactivation_date: "2024-06-08 17:34:00",
@@ -104,23 +72,8 @@ Discount.create([
     user: users[0]
   },
   {
-    name: "Biscoito integral cereale Cacau 550g",
-    description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
-    discount_type: 2,
-    activation_date: "2024-06-03 17:34:00",
-    deactivation_date: "2024-06-07 17:34:00",
-    status: 1,
-    discount_percentage: 10,
-    price_percentual: 2.55,
-    price_final: 2.30,
-    product: products[0],
-    user: users[0]
-  },
-  {
     name: "Biscoito cereale Cacau 120g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 2,
     activation_date: "2024-06-01 17:34:00",
     deactivation_date: "2024-06-09 17:34:00",
@@ -134,7 +87,6 @@ Discount.create([
   {
     name: "Biscoito cereale Cacau 170g",
     description: "O Biscoito Bauducco Cereale Cacau, Aveia e Mel Integrais é uma Fonte de fibras e com cereais integrais.",
-    image: "",
     discount_type: 2,
     activation_date: "2024-06-01 17:34:00",
     deactivation_date: "2024-06-12 17:34:00",
@@ -146,3 +98,9 @@ Discount.create([
     user: users[0]
   }
 ])
+
+puts "Salvando imagens"
+Discount.all.each do |discount|
+  image_path = Rails.root.join('app', 'assets', 'images', 'biscoito.png')
+  discount.image.attach(io: File.open(image_path), filename: 'biscoito.png')
+end 
